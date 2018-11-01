3e8496027fa48ed4e830f51e9c374a21c4095aa3
QuinnBast <Quinn.Bast@hotmail.com>
2018-11-01 00:37:21 -0600

    Added reactstrap, updated css loader, reformatted REST API to handle all requests with one endpoint

--- REST/server.py
+++ REST/server.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
-from REST.broadsoft.user.BroadsoftUserProfile import BroadsoftUserProfile
+from REST.broadsoft.BroadsoftConnector import BroadsoftConnector
+from flask_restful import Resource, Api
 import random
 
 app = Flask(__name__, static_folder="../frontend/dist", template_folder="../frontend")
+api = Api(app)
 
+api.add_resource(BroadsoftConnector, "/broadsoft")
 
 @app.route('/')
 def index():
@@ -14,11 +17,6 @@ def index():
 def hello():
     greeting_list = ['Hello', 'Hey', 'Sup', 'Yo', 'How you doing']
     return random.choice(greeting_list);
-
-
-@app.route("/user/<string:username>/profile")
-def broadsoft(username):
-    return BroadsoftUserProfile(username).getUserProfile()
 
 
 if __name__ == '__main__':
