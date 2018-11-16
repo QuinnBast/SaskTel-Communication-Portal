55be5b252c931e5a0bf1be94657e132dcbdb22ef
QuinnBast <Quinn.Bast@hotmail.com>
2018-11-16 13:44:42 -0600

    Revert "*Additional Security \n*JWT Security though CSRF protection\n*Currently In progress, authorization is not validating tokens."
    This reverts commit 4b876eb1141bb3f4e19fb4e4bce893224f029912.

--- REST/endpoints/auth/Authenticator.py
+++ REST/endpoints/auth/Authenticator.py
@@ -1,9 +1,8 @@
 from flask_restful import Resource, reqparse
 from passlib.hash import pbkdf2_sha256 as sha256
 from REST.endpoints.broadsoft.BroadsoftConnector import BroadsoftConnector
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt, set_access_cookies, set_refresh_cookies, unset_jwt_cookies)
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 import xmltodict
-from flask import jsonify
 
 class Authenticator():
 
@@ -43,18 +42,14 @@ class Authenticator():
                 # Login was successful, store the information into a JWT
                 access_token = create_access_token(identity={'id':data['username'],'pass':sha256.hash(data['password'])})
                 refresh_token = create_refresh_token(identity={'id':data['username'],'pass':sha256.hash(data['password'])})
-                response = jsonify({
+                return \
+                    {
                         'message':'User Login successful.',
                         'login':True,
-                        'broadsoft_token': str(broadsoft_token)
-                    })
-
-                # Enforces the refresh and access cookies to be stored in a cookie instead of returning the cookies to
-                # to the frontend.
-                # Also sets the CSRF double submit protection cookies in this response
-                set_refresh_cookies(response, refresh_token)
-                set_access_cookies(response, access_token)
-                return response
+                        'access_token':access_token,
+                        'refresh_token':refresh_token,
+                        'broadsoft_token': broadsoft_token
+                    }
             else:
                 return \
                     {
@@ -64,27 +59,15 @@ class Authenticator():
     class UserLogout(Resource):
         @jwt_required
         def post(self):
-            response = jsonify({'message':'Logout Successful', 'logout':True})
-            try:
-                unset_jwt_cookies(response)
-            except:
-                return {'message':'Unable to remove cookie!','logout':False}, 501
-            return response, 200
+            return {'message':'User Logout'}
 
     class TokenRefresh(Resource):
         # Can only access this path using the refresh token.
         @jwt_refresh_token_required
         def post(self):
-            # This endpoint will create and send back a new JWT access token.
-            # The JWT refresh token is valid for 30 days. This allows user's to refresh their session
             current_user = get_jwt_identity()
             access_token = create_access_token(identity=current_user)
-
-            response = jsonify({'message':'Access token refreshed.'})
-            # Set the JWT token and enforce the response in the cookie.
-            # Also sets the CSRF double submit protection cookies in this response
-            set_access_cookies(response, access_token)
-            return response, 200
+            return {'access_token':access_token}
 
     class AuthenticationTest(Resource):
         @jwt_required
