38fc3f7b2a9faffedc68eef5f57bddd9fea1b340
OmegaHelix <dakotaf@sasktel.net>
2018-11-18 15:47:44 -0600

    Routing is working, front end to handle non-api 404's

---
+++ frontend/js/interface.jsx
@@ -0,0 +1,10 @@
+import {Link, Route, Router} from "react-router-dom";
+import React, {Component} from "react";
+
+default export class Login extends Component {
+  render() {
+      return (
+         <h3> Login Page </h3>
+      );
+  }
+}
