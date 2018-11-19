4f3a91d702c0038038d22a1133f796b3c928ec3d
OmegaHelix <dakotaf@sasktel.net>
2018-11-18 22:03:15 -0600

    Routing complete, working on interface

--- REST/controllers/IndexController.py
+++ REST/controllers/IndexController.py
@@ -2,10 +2,20 @@ from REST.server import app
 import random
 from flask import render_template
 
+import os
+from flask import send_from_directory
+
+
+@app.route('/favicon.ico')
+def favicon():
+        return send_from_directory(app.root_path,
+                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 @app.route("/hello")
 def hello():
     greeting_list = ['Hello', 'Hey', 'Sup', 'Yo', 'How you doing']
-    return random.choice(greeting_list);
+    return random.choice(greeting_list)
 
 
 @app.route('/', defaults={'path': ''})
