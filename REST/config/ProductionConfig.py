f65c7a8207e96dfd6bececd359173b15cc6c99ee
Quinn Bast <Quinn.Bast@hotmail.com>
2018-12-16 16:23:42 -0600

    Config Files hide information

--- REST/config/ProductionConfig.py
+++ REST/config/ProductionConfig.py
@@ -2,4 +2,4 @@ from REST.config.Config import Config
 
 class ProductionConfig(Config):
     JWT_COOKIE_SECURE = True             # Cookies must be sent using HTTPS
-    JWT_SECRET_KEY = "6qCZ=!CZ/4T>&P/_"  # Secret value used encrypt tokens
+    JWT_SECRET_KEY = "ChangeThis"  # Secret value used encrypt tokens
