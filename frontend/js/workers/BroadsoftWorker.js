cfe345c2f5382b9405f51bb6e8ea4621ad49be15
Quinn Bast <Quinn.Bast@hotmail.com>
2018-12-22 15:25:10 -0600

    WebWorker sends broadsoft requests

--- frontend/js/workers/BroadsoftWorker.js
+++ frontend/js/workers/BroadsoftWorker.js
@@ -1,18 +1,33 @@
 
-//import UpdateQueue from "UpdateQueue"
+import UpdateQueue from "UpdateQueue"
+import BroadSoft from "../broadsoft/BroadSoft"
 
 const broadsoftWorkerCode = () => {
     // Set a timer to execute updates every minute.
     setInterval(function(){
 
-    console.log("Worker was executed.");
+        console.log("Worker was executed.");
 
-    // Get the queue
-    //if(!UpdateQueue.hasUpdates()) return;
+        // Are items in the queue?
+        if(!UpdateQueue.hasUpdates()) return;
 
-    //
+        // If items are in the queue, process them and send requests to broadsoft.
+        for(let i = 0; i < UpdateQueue.queue.length; i++){
+            // Send broadsoft requests for each update.
+            BroadSoft.sendRequest(
+                {
+                    endpoint: UpdateQueue.queue[i].endpoint,
+                    data: UpdateQueue.queue[i].data,
+                    method: UpdateQueue.queue[i].method,
+                    callback: UpdateQueue.queue[i].callback
+                }
+            );
 
-}, 60000);
+            // Remove the item from the queue.
+            UpdateQueue.queue.splice(i, 1);
+        }
+
+    }, 60000);
 
 };
 
