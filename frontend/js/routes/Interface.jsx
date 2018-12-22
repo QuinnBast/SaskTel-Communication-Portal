e42ce87907cad4ec04509331b2a29f13356d47f3
Quinn Bast <Quinn.Bast@hotmail.com>
2018-12-22 17:58:04 -0600

    Updated worker thread

--- frontend/js/routes/Interface.jsx
+++ frontend/js/routes/Interface.jsx
@@ -11,14 +11,34 @@ import CallReject from "../comps/CallReject";
 import CallLogs from "../comps/CallLogs";
 import CallDirectory from "../comps/CallDirectory";
 
-import worker_script from "../workers/BroadsoftWorker"
+import UpdateQueue from "../workers/UpdateQueue";
+import BroadSoft from "../BroadSoft/BroadSoft";
 
 export default class Interface extends Component {
 
     //Create a list of all user properties to show to the user
     constructor(props){
         super(props);
-        this.broadsoftWorker = new Worker(worker_script);
+    }
+
+    componentDidMount() {
+        setTimeout(this.checkQueue(), 60000);
+    }
+
+    checkQueue() {
+        console.log("Worker was executed.");
+
+        // Are items in the queue?
+        if (!UpdateQueue.hasUpdates()) return;
+
+        // If items are in the queue, process them and send requests to broadsoft.
+        for (let i = 0; i < UpdateQueue.queue.length; i++) {
+            // Send broadsoft requests for each update.
+                BroadSoft.sendRequest(UpdateQueue.queue[i]);
+
+            // Remove the item from the queue.
+            UpdateQueue.queue.splice(i, 1);
+        }
     }
 
     render() {
