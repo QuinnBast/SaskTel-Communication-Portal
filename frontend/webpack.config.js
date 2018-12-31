46087b504ded5353654c9c303626953546481ab9
OmegaHelix <dakotaf@sasktel.net>
2018-12-31 16:34:15 -0600

    Semantic-Less Working (No themes)
    Can also delete css in index.html, and change import in App.jsx to "semantic-ui-css/semantic.min.css" and it will work.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const config = {
     entry: {
         "indexPage": __dirname + '/js/App.jsx',
@@ -11,46 +13,64 @@ const config = {
         publicPath: '/'
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.css']
+        extensions: ['.js', '.jsx', '.css'],
+        alias: {
+            '../../theme.config$': path.join(__dirname, 'css/theme.config')
+        }
     },
     module: {
         rules: [
             {
-             test: /\.jsx?/,
-             exclude: /node_modules/,
-             loaders: 'babel-loader',
-             options: {
-             presets: ['@babel/preset-env',
-                          '@babel/react',{'plugins': ['@babel/plugin-proposal-class-properties']}
-                      ]
-            }
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                loaders: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env',
+                        '@babel/react',{'plugins': ['@babel/plugin-proposal-class-properties']}
+                    ]
+                }
             },
-             {
-                 test:/\.(s*)css$/,
-                 use:['style-loader','css-loader', 'sass-loader']
-              },
             {
-            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-            loader: require.resolve("url-loader"),
-            options: {
-                limit: 10000,
-                name: "/dist/[name].[hash:8].[ext]",
+                test:/\.(s*)css$/,
+                use:['css-loader', 'sass-loader']
             },
-        },
-        {
-            test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
-            loader: require.resolve("file-loader"),
-            options: {
-                name: "/dist/[name].[hash:8].[ext]",
+            {
+                test: /\.less$/,
+                use     : [ MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader', 'less-loader']
             },
-        }
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve("url-loader"),
+                options: {
+                    limit: 10000,
+                    name: "../dist/images/[name].[ext]",
+                },
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader",
+                options: {
+                    mimetype: "application/font-woff",
+                    limit: 10000,
+                    name: "../dist/fonts/[name].[ext]",
+                },
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader",
+                options: {
+                    limit: 10000,
+                    name: "../dist/fonts/[name].[ext]",
+                },
+            }
         ]
     },
     plugins: [
         // Copy all images/files from the assets folder into the dist/assets folder for bundling.
         new CopyWebpackPlugin([
             { from: 'assets/', to: 'assets/'}
-        ])
+        ]),
+        new MiniCssExtractPlugin()
     ]
 };
 
