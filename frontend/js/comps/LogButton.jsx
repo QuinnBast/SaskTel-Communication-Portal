278ce75dee6bcc1813b02bf6acf4fc4b2dbaaa07
OmegaHelix <dakotaf@sasktel.net>
2018-12-31 19:31:43 -0600

    Revert "Profile Dropdown Button (Mocked)"
    This reverts commit cd86f07de51b5671d5fa709b0b8cd590f1d72b5c.

--- frontend/js/comps/LogButton.jsx
+++ frontend/js/comps/LogButton.jsx
@@ -17,7 +17,9 @@ import Auth from "../router/Auth";
 
 export const LogButton = withRouter(() => (
         Auth.isAuthenticated() ? (
+            <Menu.Menu position='right'>
                 <Menu.Item as={Link} to="/login" onClick={Auth.logout}>Sign out</Menu.Item>
+            </Menu.Menu>
         ) : (
             ""
         )
