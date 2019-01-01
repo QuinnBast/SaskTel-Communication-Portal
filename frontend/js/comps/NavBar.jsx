1383be604ed57956dc6cd3d158b81e5633d43fa2
OmegaHelix <dakotaf@sasktel.net>
2018-12-31 19:31:43 -0600

    Revert "Profile Dropdown Button (Mocked)"
    This reverts commit cd86f07de51b5671d5fa709b0b8cd590f1d72b5c.

--- frontend/js/comps/NavBar.jsx
+++ frontend/js/comps/NavBar.jsx
@@ -13,15 +13,13 @@ import { Menu } from 'semantic-ui-react'
  *  Component Imports
  */
 import {LogButton} from "./LogButton"
-import ProfileDropdown from "./ProfileDropdown"
-import NavBarRight from "./NavBarRight";
 
 export default class NavBar extends Component {
     render() {
         return (
             <Menu attached inverted={true}>
                 <Menu.Item header name='Tel Port'/>
-                <NavBarRight/>
+                <LogButton/>
             </Menu>
         )
     }
