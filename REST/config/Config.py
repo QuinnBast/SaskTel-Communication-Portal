8ff8de580fe652fe85f1db126a8b107264f47680
Quinn Bast <Quinn.Bast@hotmail.com>
2019-01-05 21:41:32 -0600

    JWT Token expiry time fixed

--- REST/config/Config.py
+++ REST/config/Config.py
@@ -1,4 +1,7 @@
+import datetime
+
 class Config(object):
     JWT_COOKIE_CSRF_PROTECT = True      # Protect JWT tokens from CSRF
-    JWT_EXPIRATION_DELTA = 180          # Number of minutes to keep tokens valid for
+    JWT_EXPIRATION_DELTA = datetime.timedelta(hours=4)          # Number of hours to keep tokens valid for
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=4)      # Number of hours to keep access tokens valid for
     JWT_TOKEN_LOCATION = ['cookies']    # Indicates where to store JWTs for user identity
