7cc34cf4b3117a96d610cc8c50a19b068359205c
Quinn Bast <Quinn.Bast@hotmail.com>
2019-01-05 18:16:30 -0600

    Call Waiting UI development

--- frontend/js/comps/CallWaiting.jsx
+++ frontend/js/comps/CallWaiting.jsx
@@ -12,34 +12,57 @@ import AccordionWrap from "./AccordionWrap"
  *  REST API Imports
  */
 import BroadSoft from "../broadsoft/BroadSoft";
+import CallWaitingData from "./CallWaitingData";
 
 export default class CallWaiting extends React.Component {
 
     constructor(props) {
         super(props);
         this.loadAsync();
+        this.state = {
+            unauthorized: false,
+            data: [],
+            status: "loading"
+        }
     }
 
 // Asynchronous function that updates the object.
     loadAsync(){
+        let self = this;
         BroadSoft.sendRequest({
             endpoint: "/user/<user>/services/CallWaiting",
             success: function(response) {
-                $("#CallWaiting").get(0).innerHTML = JSON.stringify(response);
+                let data = <CallWaitingData key={"CallingNumberData"} dataformat={response} endpoint={"/user/<user>/services/CallWaiting"}/>;
+                self.setState((prevState) => ({data: [...prevState.data, data], status: "ready"}));
             },
             error: function(response) {
                 // User does not have access to the endpoint.
+                self.setState({unauthorized: true});
             }
         });
     }
 
     render() {
-        return (
-            <AccordionWrap title={"Call Waiting"} description={"Allows you to receive another call when you are already on the phone."}>
-                <div>
-                    <div id={"CallWaiting"}>Loading...</div>
-                </div>
-            </AccordionWrap>
-        )
+        if(this.state.status === "loading") {
+            return (
+                <AccordionWrap title={"Call Waiting"}
+                               description={"Allows you to receive another call when you are already on the phone."}
+                               unauthorized={this.state.unauthorized}>
+                    <div>
+                        <div id={"CallWaiting"}>Loading...</div>
+                    </div>
+                </AccordionWrap>
+            )
+        } else if(this.state.status === "ready"){
+           return (
+                <AccordionWrap title={"Call Waiting"}
+                               description={"Allows you to receive another call when you are already on the phone."}
+                               unauthorized={this.state.unauthorized}>
+                    <div>
+                        {this.state.data}
+                    </div>
+                </AccordionWrap>
+            )
+        }
     }
 }
