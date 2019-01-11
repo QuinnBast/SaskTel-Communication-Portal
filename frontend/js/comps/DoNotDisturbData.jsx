e437d3953ebcd3f14ee932111f7c0b30a26e7c62
Quinn Bast <Quinn.Bast@hotmail.com>
2019-01-11 17:58:53 -0600

    Fixed jQuery bug highlighting all data red

--- frontend/js/comps/DoNotDisturbData.jsx
+++ frontend/js/comps/DoNotDisturbData.jsx
@@ -66,8 +66,8 @@ export default class DoNotDisturbData extends React.Component {
             },
             error: function(response){
                 // Permanently change background to red to indicate error to user.
-                jQuery("#DoNotDisturbActive").get(0).style.background = '#e74c3c';
-                jQuery("#DoNotDisturbRingSplash").get(0).style.background = '#e74c3c';
+                jQuery("#DoNotDisturbActive").get(0).style.background = jQuery("#DoNotDisturbActive").get(0).style.background === '#fff7e6' ? '#e74c3c' : "";
+                jQuery("#DoNotDisturbRingSplash").get(0).style.background = jQuery("#DoNotDisturbRingSplash").get(0).style.background === '#fff7e6' ? '#e74c3c' : "";
                 // Reset the state of the component by fetching the current state.
             }
         };
