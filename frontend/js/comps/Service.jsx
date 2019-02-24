ef530473ca749b0cbc9f09e2f189dfab4ab3f9a4
Quinn Bast <Quinn.Bast@hotmail.com>
2019-02-23 18:36:34 -0600

    Updated toggle button CSS

--- frontend/js/comps/Service.jsx
+++ frontend/js/comps/Service.jsx
@@ -110,7 +110,19 @@ export default class Service extends React.Component {
             toggle = <Container>
                     <div>
                         <h5>Active</h5>
-                        <Switch id={this.props.name.replace(/\s+/g, '') + "Toggle"} onChange={this.toggle} checked={this.state.active}/>
+                        <Switch
+                            id={this.props.name.replace(/\s+/g, '') + "Toggle"}
+                            onChange={this.toggle} checked={this.state.active}
+                            onColor="#86d3ff"
+                            onHandleColor="#2693e6"
+                            handleDiameter={30}
+                            uncheckedIcon={false}
+                            checkedIcon={false}
+                            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                            height={20}
+                            width={48}
+                        />
                     </div>
                 </Container>;
         }
@@ -183,7 +195,18 @@ export default class Service extends React.Component {
 
         let toggle = null;
         if(this.props.hasToggle){
-            toggle = <Switch id={this.props.name.replace(/\s+/g, '') + "Toggle"} onChange={this.toggle} checked={this.state.active}/>;
+            toggle = <Switch
+                id={this.props.name.replace(/\s+/g, '') + "Toggle"}
+                onChange={this.toggle} checked={this.state.active}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={30}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48}/>;
         }
 
         return (
--- frontend/js/comps/XmlEditable.jsx
+++ frontend/js/comps/XmlEditable.jsx
@@ -118,7 +118,19 @@ export default class XmlEditable extends React.Component {
                 return(
                     <Container id={this.props.name.replace(/\s+/g, '') + "EditableBool"} style={padding}>
                         <div>{name}</div>
-                        <div><Switch onChange={this.inputChange} checked={this.state.value}/></div>
+                        <div><Switch
+                            onChange={this.inputChange}
+                            checked={this.state.value}
+                            onColor="#86d3ff"
+                            onHandleColor="#2693e6"
+                            handleDiameter={30}
+                            uncheckedIcon={false}
+                            checkedIcon={false}
+                            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                            height={20}
+                            width={48}/>
+                        </div>
                         {titlePopover}
                     </Container>
                 );
