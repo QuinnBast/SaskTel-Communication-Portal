54d084a05ff29ab7f038d884d6e481d27c3c7e15
Quinn Bast <Quinn.Bast@hotmail.com>
2019-04-04 18:50:44 -0600

    AudioController and App tests

--- frontend/tests/jest/App.test.jsx
+++ frontend/tests/jest/App.test.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../../js/App';
+import history from "../../js/router/history";
 jest.mock('../../js/comps/AudioController', () => () => 'SomeComponent');
 jest.mock('../../css/main.css', () => () => 'SomeComponent');
 jest.mock('bootstrap/dist/css/bootstrap.min.css', () => () => 'SomeComponent');
+jest.mock('react-dom', () => ({
+     render: jest.fn(),
+}));
 
 describe("App", () => {
 
@@ -14,4 +18,14 @@ describe("App", () => {
 
         let wrapper = shallow(<App/>);
     });
+
+    it('loads invalid pages with 404', () => {
+
+
+        document.body = document.createElement("body");
+        document.body.innerHTML = "<div id='content'/>";
+
+        let wrapper = shallow(<App/>);
+        history.push("/somefakeurl");
+    })
 });
--- frontend/tests/jest/AudioController.test.jsx
+++ frontend/tests/jest/AudioController.test.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import AudioController from '../../js/comps/AudioController';
 
-jest.autoMock(true);
-
 describe("App", () => {
 
-    it('loads', () => {
-        let wrapper = shallow(<AudioController/>);
+    it('constructs', () => {
+        let ac = AudioController;
     });
+
+    it('plays audio', () => {
+        AudioController.play('tone_click');
+        AudioController.play('tone_0');
+        AudioController.play('tone_1');
+        AudioController.play('tone_2');
+        AudioController.play('tone_3');
+        AudioController.play('tone_4');
+        AudioController.play('tone_5');
+        AudioController.play('tone_6');
+        AudioController.play('tone_7');
+        AudioController.play('tone_8');
+        AudioController.play('tone_9');
+        AudioController.play('tone_busy');
+        AudioController.play('tone_pound');
+        AudioController.play('tone_ringback');
+        AudioController.play('tone_star');
+        AudioController.play('tone_ringtone');
+        AudioController.play('tone_ringtone');
+        AudioController.stop('tone_ringtone');
+    })
 });
